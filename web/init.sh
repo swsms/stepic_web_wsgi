dd86f1ddf8d28f4f88180013a574b9f20369e448
art <swsms@mail.ru>
2016-11-15 21:01:36 +0500

    FIX init.sh

--- web/init.sh
+++ web/init.sh
@@ -1,4 +1,4 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/art/Mooc/stepic_web_wsgi/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo gunicorn -w 4 -b 0.0.0.0:8080 wsgi_server:app
